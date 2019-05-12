a90d7e840f22768b2ed727e7804022b18e15967d
Rahul Bharati <rahul@rahul-pc.localdomain>
2019-05-12 16:44:48 +0530

    shopping cart functionality added

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 #media settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# Session Settings
+CART_SESSION_ID = 'cart'
