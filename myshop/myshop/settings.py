548828f77747017cbc24d240ca99bba4b3e091b7
Rahul Bharati <rahulrbharati@gmail.com>
2019-07-07 13:25:53 +0530

    celery added

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig'
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_preprocessor.cart'
             ],
         },
     },
@@ -128,3 +130,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Session Settings
 CART_SESSION_ID = 'cart'
+
+#email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
